11611340143754e77372080ed5edabf083108c6e
FordY7014 <yangfan7014@gmail.com>
2022-07-23 19:29:34 +1200

    new_page

---
+++ AAA/data/TRIAL_TABLE_2.js
@@ -0,0 +1,9 @@
+var json_TRIAL_TABLE_2 = {
+"type": "FeatureCollection",
+"name": "TRIAL_TABLE_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "aaa": "2", "BBB": 2.0 }, "geometry": { "type": "Point", "coordinates": [ 174.678207684655064, -36.890134398418915 ] } },
+{ "type": "Feature", "properties": { "aaa": "3", "BBB": 4.0 }, "geometry": { "type": "Point", "coordinates": [ 174.677319787766862, -36.890361741816541 ] } }
+]
+}
